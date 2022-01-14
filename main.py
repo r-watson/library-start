b4278019cce2676f5cbc1506cd42257cacd02f9e
r-watson <rtwatson86@gmail.com>
2022-01-13 23:04:50 -0500

    sqlite database created

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 db = sqlite3.connect("books-collection.db")
 cursor = db.cursor()
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+db.commit()
 
 all_books = []
 
@@ -33,5 +38,3 @@ def add():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
